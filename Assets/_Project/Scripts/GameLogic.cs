25304165d578ed926a393475a3e0d779ff5c38ec
Stanislav Kozlov <llarean@ya.ru>
2024-11-07 18:13:19 +0300

    Adding questions

--- Assets/_Project/Scripts/GameLogic.cs
+++ Assets/_Project/Scripts/GameLogic.cs
@@ -11,6 +11,7 @@ namespace millionaire
         [SerializeField] private QuestionsData _hardQuestions;
 
         private QuestionData _currentQuestion;
+        private int _currentQuestionIndex;
         
         private void Start()
         {
@@ -36,6 +37,16 @@ namespace millionaire
         {
             _currentQuestion = _easyQuestions.Questions[Random.Range(0, _easyQuestions.Questions.Count)];
             _quizScreen.ShowQuestionData(_currentQuestion);
+        }
+
+        private void GetQuestion()
+        {
+            _currentQuestion = _currentQuestionIndex switch
+            {
+                <= 5 => _easyQuestions.Questions[Random.Range(0, _easyQuestions.Questions.Count)],
+                <= 10 => _middleQuestions.Questions[Random.Range(0, _middleQuestions.Questions.Count)],
+                _ => _hardQuestions.Questions[Random.Range(0, _hardQuestions.Questions.Count)]
+            };
         }
     }
 }
